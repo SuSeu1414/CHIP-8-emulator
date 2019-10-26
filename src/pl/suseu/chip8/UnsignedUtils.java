cc2ee0c53d39baedf5ce7d2e158172f30d588a49
BuildTools <szymonwitek02@gmail.com>
2019-10-26 13:23:34 +0200

    Add more unsigned ints

--- src/pl/suseu/chip8/UnsignedUtils.java
+++ src/pl/suseu/chip8/UnsignedUtils.java
@@ -2,7 +2,19 @@ package pl.suseu.chip8;
 
 public class UnsignedUtils {
 
+    public static int uint(boolean b) {
+        return b ? 1 : 0;
+    }
+
     public static int uint(byte b) {
         return b & 0xFF;
+    }
+
+    public static int uint(short s) {
+        return s & 0xFFFF;
+    }
+
+    public static int uint(char c) { // char can be used as unsigned short
+        return c;
     }
 }
