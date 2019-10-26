724a62fd904cf3a38cfe44eaa931cb4bbba54bca
BuildTools <szymonwitek02@gmail.com>
2019-10-26 17:08:42 +0200

    Add rom loading to memory

--- src/pl/suseu/chip8/Emulator.java
+++ src/pl/suseu/chip8/Emulator.java
@@ -1,5 +1,7 @@
 package pl.suseu.chip8;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +21,35 @@ public class Emulator {
     private int sp; // stack pointer
     private byte[] keys = new byte[16]; // HEX based keypad
 
-    public static void main(String[] args) {
+    public Emulator() throws Exception{
+        initialize();
+    }
 
+    private void initialize() throws Exception{
+        pc = 0x200; // Program counter always starts at 0x200 (512)
+
+        //TODO: Load fontset
+
+        byte[] binary = loadBinary("roms/Pong.c8");
+        for (int i = 0; i < binary.length; i++) {
+            memory[i+512] = binary[i];
+        }
+    }
+
+    private void emulateCycle(){
+
+    }
+
+    private byte[] loadBinary(String filename) throws Exception {
+        System.out.println("Loading " + filename + " ...");
+        DataInputStream in = new DataInputStream(new FileInputStream(filename));
+        byte[] bytes = new byte[in.available()];
+        int loadedBytes = in.read(bytes);
+        System.out.println("Loaded " + loadedBytes + " bytes.");
+        return bytes;
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Emulator();
     }
 }
